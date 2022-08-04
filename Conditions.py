1022452c870fec23c6be49b6eb8e0bc7fe1ad760
Devashish <devashishtest10@gmail.com>
2022-08-04 16:01:28 +0100

    Second commit

--- Conditions.py
+++ Conditions.py
@@ -5,6 +5,8 @@ Greeting = "Good Morning"
 if Greeting == "Good Morning":  # Colon used in place of curly brackets
     print("Condition matches")
     print("Perfect statement")
+    print("Perfectss statement")
+    print("Perfectsss statement")
 else:
     print("Condition not matches")  # Indentation is very important
     print("Not a Perfect statement")
